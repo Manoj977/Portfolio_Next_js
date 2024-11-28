6922305ba18f39d80236a6a8d4e4e071f3eac65e
Manoj S <73774886+manoj-sharvan@users.noreply.github.com>
2024-11-28 18:28:31 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
                   Resume <LinkArrow className={'w-6 ml-1'} />
                 </Link>
                 <Link
-                  href={'mailto:abcd@gmail.com'}
+                  href={'mailto:manojsharvan@gmail.com'}
                   target={'_blank'}
                   className="ml-4 text-lg font-medium capitalize text-dark underline"
                 >
